dce830b17506b157b11edec96f464044594aaf2f
William Roger <williamroger.frontend@gmail.com>
2019-09-19 23:14:48 -0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
+  { path: '', redirectTo: 'register', pathMatch: 'full'},
+    // path: '',
+    // loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  { path: 'register', loadChildren: './register/register.module#RegisterPageModule' }
 ];
 @NgModule({
   imports: [
