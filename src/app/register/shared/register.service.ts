86f8db64d8b3c9ad05d0e03155fb94c2196e277e
William Roger <williamroger.frontend@gmail.com>
2019-09-20 03:42:23 -0300

    Ainda não funciona

--- src/app/register/shared/register.service.ts
+++ src/app/register/shared/register.service.ts
@@ -6,15 +6,20 @@ import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { CustomerModel } from '../shared/customer';
 
+const headers = new HttpHeaders({
+ 
+});
+
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterService {
+  private header = new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': 'http://localhost:8100', 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS'});
 
   constructor(public http: HttpClient) { }
 
   createCustumer(customer: CustomerModel): Observable<CustomerModel> {
-    return this.http.post('api/customer/insertcustomer', customer).pipe(
+    return this.http.post('http://localhost:8080/customer/insertcustomer', customer, {headers: this.header}).pipe(
       catchError(this.handleError),
       map(this.jsonDataToCustomer)
     )
